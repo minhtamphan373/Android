a502fb5e383cb3ad807ade5225f7b96e9efd841b
minhtamphan373 <minhtamphan373@gmail.com>
2023-04-01 13:46:10 +0700

    update

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Component } from 'react';
-import { StyleSheet, Text, Image, TouchableHighlight, TouchableNativeFeedback, 
-  TouchableOpacity, TouchableWithoutFeedback, View, FlatList, TextInput } from 'react-native';
+import { StyleSheet, Text, ImageBackground, Image, TouchableHighlight, TouchableNativeFeedback, 
+  TouchableOpacity, TouchableWithoutFeedback, View, TextInput, ScrollView, span } from 'react-native';
 
 
 class App extends Component {
@@ -14,51 +14,59 @@ class App extends Component {
       count: this.state.count + 1,
     });
   };
+  handlePress = () => {
+    alert('Button pressed!');
+  }
 
   render(){
     const {count} = this.state;
-    const handlePress = () => {
-      console.log('Button pressed!');
-    }
+    
     return(
 
-      <View style={styles.container}>
-        <View style={styles.imgContainer}>
-          <Image
-          source={{
-            uri: "https://i.pinimg.com/564x/d8/3f/52/d83f52e6eca309d746545bfaf54000f2.jpg"
-          }}
-          style={styles.img}
-          />
-        </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <ImageBackground source={{uri:"https://i.pinimg.com/564x/71/bb/19/71bb199e8827b479dd019bbccb951f04.jpg"}} 
+            resizeMode="cover" style={styles.imgBackground}> 
+            <View style={styles.imgContainer}>
+              <Image
+              source={{
+                uri: "https://i.pinimg.com/564x/12/27/0d/12270dd8915987dbbb43679d3d7efc7a.jpg"
+              }}
+              style={styles.img}
+              />
+            </View>
 
+            <View style={styles.countContainer}>
+              <TextInput style={styles.textIn}> Text Input</TextInput>
+              <TextInput placeholder='Here is placeholder' style={styles.textHolder}></TextInput>
+              <Text style={styles.textTouch}>Count: {count}</Text>
+            </View>
+
+              <View style={styles.touchable}>
+            <TouchableOpacity style={styles.button} onPress={this.onPress}>
+              <Text style={styles.textTouch}>TouchableOpacity</Text>
+            </TouchableOpacity>
+
+            <TouchableHighlight style={styles.highlightButton} onPress = {this.handlePress} underlayColor="#a64b23" >
+              <Text style={styles.textTouch}>TouchableHighlight</Text>
+            </TouchableHighlight>
+
+            <TouchableNativeFeedback onPress={this.onPress}>
+            <View style={styles.button}>
+              <Text style={styles.textTouch}>TouchableNativeFeedback</Text>
+              </View>
+            </TouchableNativeFeedback>
+
+            <TouchableWithoutFeedback onPress={this.onPress}>
+            <View style={styles.button}>
+              <Text style={styles.textTouch}>TouchableWithoutFeedback</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            </View>
+          </ImageBackground>
+        </View>
+      </ScrollView>
       
-        <View style={styles.countContainer}>
-          <TextInput style={styles.textIn}> Type Here</TextInput>
-          <Text style={styles.textTouch}>Count: {count}</Text>
-          
-        </View>
-
-        <TouchableOpacity style={styles.button} onPress={this.onPress}>
-          <Text style={styles.textTouch}>TouchableOpacity</Text>
-        </TouchableOpacity>
-
-        <TouchableHighlight style={styles.highlightButton} onPress = {this.onPress} underlayColor="#a64b23" >
-          <Text style={styles.textTouch}>TouchableHighlight</Text>
-        </TouchableHighlight>
-
-        <TouchableNativeFeedback onPress={this.onPress}>
-        <View style={styles.button}>
-          <Text style={styles.textTouch}>TouchableNativeFeedback</Text>
-          </View>
-        </TouchableNativeFeedback>
-
-        <TouchableWithoutFeedback onPress={this.onPress}>
-        <View style={styles.button}>
-          <Text style={styles.textTouch}>TouchableWithoutFeedback</Text>
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
 
       
       );
@@ -72,11 +80,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingHorizontal: 10, 
+    // paddingHorizontal: 30, 
     backgroundColor:'#f1bc80'
   },
 
   imgContainer: {
+    paddingTop: 100,
     justifyContent: 'center',
     alignItems:'center',
   },
@@ -87,6 +96,16 @@ const styles = StyleSheet.create({
     fontSize: 30,
     paddingBottom:10,
   },
+
+  textHolder: {
+    color: '#a64b23',
+    fontSize: 20,
+    paddingBottom:10,
+  },
+
+ touchable: {
+  paddingHorizontal: 30,
+ },
 
   textTouch: {
     color:'#8d6b4e',
@@ -125,9 +144,14 @@ const styles = StyleSheet.create({
   },
 
   img: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
+    width: 250,
+    height: 250,
+    borderRadius: 200,
+  },
+
+  imgBackground: {
+    flex: 1,
+    justifyContent: 'center',
   }
 
 });
